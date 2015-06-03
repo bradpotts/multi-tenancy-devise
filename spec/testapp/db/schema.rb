7bc9ed2e3e800b5470eb0731817e407b11a488f6
BradPotts <bradley.j.potts@gmail.com>
2015-06-03 04:59:39 +0000

    Add codeclimate for testing.

--- spec/testapp/db/schema.rb
+++ spec/testapp/db/schema.rb
@@ -11,12 +11,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150603030846) do
+ActiveRecord::Schema.define(version: 20150603034836) do
 
   create_table "subengine_accounts", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "subengine_users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "subengine_users", ["email"], name: "index_subengine_users_on_email", unique: true
+  add_index "subengine_users", ["reset_password_token"], name: "index_subengine_users_on_reset_password_token", unique: true
 
 end
