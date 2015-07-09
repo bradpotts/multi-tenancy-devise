afe1498386cf0a34414da9cb3eecc1e4a27867ef
BradPotts <bradley.j.potts@gmail.com>
2015-07-09 04:07:30 +0000

    Add scoped_to extention. Make it easier to install in models.

---
+++ lib/subengine/scoped_to.rb
@@ -0,0 +1,7 @@
+module Subengine
+  module ScopedTo
+    def scoped_to(account)
+       where(:account_id => account.id)
+    end
+  end
+end
--- spec/test_app/app/models/thing.rb
+++ spec/test_app/app/models/thing.rb
@@ -1,5 +1,3 @@
 class Thing < ActiveRecord::Base
-  def self.scoped_to(account)
-    where(:account_id => account.id)
-  end
+  extend Subengine::ScopedTo
 end
