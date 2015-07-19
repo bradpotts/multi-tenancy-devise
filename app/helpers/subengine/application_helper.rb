b7a41c7cb21470f69ec830cdc8c4f393242b0676
BradPotts <bradley.j.potts@gmail.com>
2015-07-19 02:53:11 +0000

    Updated alter flash system.

--- app/helpers/subengine/application_helper.rb
+++ app/helpers/subengine/application_helper.rb
@@ -2,16 +2,14 @@ module Subengine
 	module ApplicationHelper
 
 		# Bootstrap Notification System
-		def bootstrap_class_for(flash_type)
-			case flash_type
+		def flash_class(level)
+			case level.to_sym
 				when :notice then "alert alert-info"
 				when :success then "alert alert-success"
 				when :error then "alert alert-danger"
 				when :alert then "alert alert-warning"
-			else
-				flash_type.to_s
-			end
-		end
-
+		  end
+    end
+          
 	end
 end
