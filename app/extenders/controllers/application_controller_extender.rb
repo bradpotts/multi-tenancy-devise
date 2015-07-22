e700ba4ced1f779c6dfd9fcbbb2294975ead1ef4
BradPotts <bradley.j.potts@gmail.com>
2015-07-22 04:16:56 +0000

    Removed old code left over from warden project.

--- app/extenders/controllers/application_controller_extender.rb
+++ app/extenders/controllers/application_controller_extender.rb
@@ -6,30 +6,4 @@
 
   helper_method :current_account
 
-  # def current_user
-  #   if user_signed_in?
-  #     @current_user ||= begin
-  #       user_id = env["warden"].user(:scope => :user)
-  #       Subengine::User.find(user_id)
-  #     end
-  #   end
-  # end
-  # helper_method :current_user
-
-  # def user_signed_in?
-  #   env["warden"] && env["warden"].authenticated?(:user)
-  # end
-  # helper_method :user_signed_in?
-
-  # def authenticate_user!
-  #   unless user_signed_in?
-  #     flash[:info] = "Please sign in."
-  #     redirect_to '/sign_in'
-  #   end
-  # end
-
-  # def force_authentication!(user)
-  #   env["warden"].set_user(user, :scope => :user)
-  #   # env["warden"].set_user(account, :scope => :account)
-  # end
 end
