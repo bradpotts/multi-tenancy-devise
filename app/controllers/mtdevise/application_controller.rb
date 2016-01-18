6cb320400cf7b864244cd1c8641f77391e9a0f93
BradPotts <bradley.j.potts@gmail.com>
2016-01-18 05:01:46 +0000

    Updated devise routes removed old redirection.

--- app/controllers/mtdevise/application_controller.rb
+++ app/controllers/mtdevise/application_controller.rb
@@ -2,42 +2,54 @@ module Mtdevise
 
 # class ApplicationController < ActionController::Base
 	class ApplicationController < ::ApplicationController
+
+		# Secirity and Filters
 		protect_from_forgery with: :null_session
 		before_action :check_subdomain
 
+		# Omniauth
 		def after_sign_in_path_for(resource)
 			request.env['omniauth.origin'] || stored_location_for(resource) || mtdevise.accounts_path
 		end
 
-		# Starting to Add User Helpers
+		# Helpers for Current Account and Users
 		def current_user?(user)
 			user == current_user
 		end
 		helper_method :current_user
-
 		def current_account?(account)
 			account == current_account
 		end
 		helper_method :current_account 
 
+		# Devise Custom Layouts
+		config.to_prepare do
+			Devise::SessionsController.layout "mtdevise/accounts"
+			Devise::RegistrationsController.layout proc{ |controller| user_signed_in? ? "application" : "mtdevise/accountsedit" }
+			Devise::ConfirmationsController.layout "mtdevise/accounts"
+			Devise::UnlocksController.layout "mtdevise/accounts"
+			Devise::PasswordsController.layout "mtdevise/accounts"
+		end
+
 		private
 
+		# Logged in redirect
 		def redirect_logged_in_users_to_account_page
 			redirect_to mtdevise.accounts_path if user_signed_in?
 		end
 
+		# Subdomain Checker
 		def check_subdomain
 			if request.subdomain.present? && Account.where(subdomain: request.subdomain).blank?
 				redirect_to mtdevise.root_url(subdomain: false), notice: "Subdomain Doesn't Exist. Would you like to Register."
 			end
 		end
 
+		# Resolve Layouts
 		def layouts_rsolver
 			case action_name
 				when "index"
 					"mtdevise/accountsindex"
-				when devise_controller? && resource_name == :user && action_name == "edit"
-					"mtdevise/accountsedit"
 				else
 					"mtdevise/accounts"
 			end
