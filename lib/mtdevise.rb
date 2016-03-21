697243403e3d1f692d20ac4c237fbde1002f7e47
Brad Potts <bradley.j.potts@gmail.com>
2016-03-21 15:10:12 +0000

    Updated mtdevise library.

--- lib/mtdevise.rb
+++ lib/mtdevise.rb
@@ -1,10 +1,4 @@
 require "mtdevise/engine"
 
 module Mtdevise
-	# TODO check this
-	# class Engine < ::Rails::Engine
-	#   config.to_prepare do
-	#     Devise::SessionsController.layout "layout_for_sessions_controller"
-	#   end
-	# end
 end
